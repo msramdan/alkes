0be61151d605ce68d7d9e04d205fb8243ebab4a1
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-03-25 15:47:25 +0700

    add profile page

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -9,11 +9,13 @@
                 <!-- User Information-->
                 <div class="card user-info-card">
                     <div class="card-body p-4 d-flex align-items-center">
-                        <div class="user-profile me-3"><img src="{{ asset('frontend/img/bg-img/9.jpg') }}" alt="">
+                        <div class="user-profile me-3">
+                            <img src="{{ Storage::url('public/img/teknisi/' . get_data_teknisi()->photo) }}" alt="avatar">
+
                         </div>
                         <div class="user-info">
                             <p class="mb-0 text-dark">@Pelaksana Teknis</p>
-                            <h5 class="mb-0">Suha Jannat</h5>
+                            <h5 class="mb-0">{{ get_data_teknisi()->nama }}</h5>
                         </div>
                     </div>
                 </div>
@@ -23,36 +25,36 @@
                         <div class="single-profile-data d-flex align-items-center justify-content-between">
                             <div class="title d-flex align-items-center"><i class="fa-solid fa-user"></i><span>Nama</span>
                             </div>
-                            <div class="data-content">SUHA JANNAT</div>
+                            <div class="data-content">{{ get_data_teknisi()->nama }}</div>
                         </div>
                         <div class="single-profile-data d-flex align-items-center justify-content-between">
                             <div class="title d-flex align-items-center"><i
                                     class="fa-solid fa-person-half-dress"></i><span>Jenis
                                     Kelamin</span>
                             </div>
-                            <div class="data-content">SUHA JANNAT</div>
+                            <div class="data-content">{{ get_data_teknisi()->jenis_kelamin }}</div>
                         </div>
                         <div class="single-profile-data d-flex align-items-center justify-content-between">
                             <div class="title d-flex align-items-center"><i class="fa-solid fa-phone"></i><span>No
                                     Telpon</span>
                             </div>
-                            <div class="data-content">083874731480</div>
+                            <div class="data-content">{{ get_data_teknisi()->no_telpon }}</div>
                         </div>
                         <div class="single-profile-data d-flex align-items-center justify-content-between">
                             <div class="title d-flex align-items-center"><i
                                     class="fa-solid fa-envelope"></i><span>Email</span></div>
-                            <div class="data-content">care@example.com </div>
+                            <div class="data-content">{{ get_data_teknisi()->email }} </div>
                         </div>
                         <div class="single-profile-data d-flex align-items-center justify-content-between">
                             <div class="title d-flex align-items-center"><i
                                     class="fa-solid fa-location-dot"></i><span>Tempat Lahir</span></div>
-                            <div class="data-content">Bogor</div>
+                            <div class="data-content">{{ get_data_teknisi()->tempat_lahir }}</div>
                         </div>
 
                         <div class="single-profile-data d-flex align-items-center justify-content-between">
                             <div class="title d-flex align-items-center"><i class="fa fa-calendar"
                                     aria-hidden="true"></i><span>Tangggal Lahir</span></div>
-                            <div class="data-content">11-01-1995</div>
+                            <div class="data-content">{{ get_data_teknisi()->tangal_lahir }}</div>
                         </div>
                         <!-- Edit Profile-->
                         <div class="edit-profile-btn mt-3"><a class="btn btn-primary w-100" href="edit-profile.html"><i
