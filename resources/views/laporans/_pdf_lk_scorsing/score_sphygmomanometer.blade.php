cfcee56aa30eca8b9a9fb18aee35fe0865566e3d
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-09-22 22:44:09 +0700

    update

--- resources/views/laporans/_pdf_lk_scorsing/score_sphygmomanometer.blade.php
+++ resources/views/laporans/_pdf_lk_scorsing/score_sphygmomanometer.blade.php
@@ -240,68 +240,141 @@ $scoreLajuHasil = $lajuHasil == 'Lulus' ? 20 : 0;
 $laporan_akurasi_tekanan = DB::table('laporan_akurasi_tekanan')
     ->where('no_laporan', $laporan->no_laporan)
     ->first();
+$data_sertifikat_akurasi_tekanan = json_decode($laporan_akurasi_tekanan->data_sertifikat);
 // naik
 $percobaan0_1_naik = $laporan_akurasi_tekanan->percobaan0_1_naik;
 $percobaan0_2_naik = $laporan_akurasi_tekanan->percobaan0_2_naik;
 $percobaan0_3_naik = $laporan_akurasi_tekanan->percobaan0_3_naik;
 $meanNaik0 = ($percobaan0_1_naik + $percobaan0_2_naik + $percobaan0_3_naik) / 3;
+$meanterkoreksi0 = $data_sertifikat_akurasi_tekanan->intercept_naik + $data_sertifikat_akurasi_tekanan->x_variable_naik * $meanNaik0;
+$koreksi = $meanNaik0 - 0;
+// stdev
+$arr = [];
+array_push($arr, $percobaan0_1_naik, $percobaan0_2_naik, $percobaan0_3_naik);
+$stdev = standard_deviation($arr);
 
 $percobaan50_1_naik = $laporan_akurasi_tekanan->percobaan50_1_naik;
 $percobaan50_2_naik = $laporan_akurasi_tekanan->percobaan50_2_naik;
 $percobaan50_3_naik = $laporan_akurasi_tekanan->percobaan50_3_naik;
 $meanNaik50 = ($percobaan50_1_naik + $percobaan50_2_naik + $percobaan50_3_naik) / 3;
+$meanterkoreksi50 = $data_sertifikat_akurasi_tekanan->intercept_naik + $data_sertifikat_akurasi_tekanan->x_variable_naik * $meanNaik50;
+// stdev
+$arr2 = [];
+array_push($arr2, $percobaan50_1_naik, $percobaan50_2_naik, $percobaan50_3_naik);
+$stdev2 = standard_deviation($arr2);
+$koreksi2 = $meanNaik50 - 50;
 
 $percobaan100_1_naik = $laporan_akurasi_tekanan->percobaan100_1_naik;
 $percobaan100_2_naik = $laporan_akurasi_tekanan->percobaan100_2_naik;
 $percobaan100_3_naik = $laporan_akurasi_tekanan->percobaan100_3_naik;
 $meanNaik100 = ($percobaan100_1_naik + $percobaan100_2_naik + $percobaan100_3_naik) / 3;
+$meanterkoreksi100 = $data_sertifikat_akurasi_tekanan->intercept_naik + $data_sertifikat_akurasi_tekanan->x_variable_naik * $meanNaik100;
+// stdev
+$arr3 = [];
+array_push($arr3, $percobaan100_1_naik, $percobaan100_2_naik, $percobaan100_3_naik);
+$stdev3 = standard_deviation($arr3);
+$koreksi3 = $meanNaik100 - 100;
 
 $percobaan150_1_naik = $laporan_akurasi_tekanan->percobaan150_1_naik;
 $percobaan150_2_naik = $laporan_akurasi_tekanan->percobaan150_2_naik;
 $percobaan150_3_naik = $laporan_akurasi_tekanan->percobaan150_3_naik;
 $meanNaik150 = ($percobaan150_1_naik + $percobaan150_2_naik + $percobaan150_3_naik) / 3;
-
+$meanterkoreksi150 = $data_sertifikat_akurasi_tekanan->intercept_naik + $data_sertifikat_akurasi_tekanan->x_variable_naik * $meanNaik150;
+// stdev
+$arr4 = [];
+array_push($arr4, $percobaan150_1_naik, $percobaan150_2_naik, $percobaan150_3_naik);
+$stdev4 = standard_deviation($arr4);
+$koreksi4 = $meanNaik150 - 150;
 
 $percobaan200_1_naik = $laporan_akurasi_tekanan->percobaan200_1_naik;
 $percobaan200_2_naik = $laporan_akurasi_tekanan->percobaan200_2_naik;
 $percobaan200_3_naik = $laporan_akurasi_tekanan->percobaan200_3_naik;
 $meanNaik200 = ($percobaan200_1_naik + $percobaan200_2_naik + $percobaan200_3_naik) / 3;
+$meanterkoreksi200 = $data_sertifikat_akurasi_tekanan->intercept_naik + $data_sertifikat_akurasi_tekanan->x_variable_naik * $meanNaik200;
+// stdev
+$arr5 = [];
+array_push($arr5, $percobaan200_1_naik, $percobaan200_2_naik, $percobaan200_3_naik);
+$stdev5 = standard_deviation($arr5);
+$koreksi5 = $meanNaik200 - 200;
 
 $percobaan250_1_naik = $laporan_akurasi_tekanan->percobaan250_1_naik;
 $percobaan250_2_naik = $laporan_akurasi_tekanan->percobaan250_2_naik;
 $percobaan250_3_naik = $laporan_akurasi_tekanan->percobaan250_3_naik;
 $meanNaik250 = ($percobaan250_1_naik + $percobaan250_2_naik + $percobaan250_3_naik) / 3;
+$meanterkoreksi250 = $data_sertifikat_akurasi_tekanan->intercept_naik + $data_sertifikat_akurasi_tekanan->x_variable_naik * $meanNaik250;
+// stdev
+$arr6 = [];
+array_push($arr6, $percobaan250_1_naik, $percobaan250_2_naik, $percobaan250_3_naik);
+$stdev6 = standard_deviation($arr6);
+$koreksi6 = $meanNaik250 - 250;
 
 // turun
-$percobaan0_1_turun = $laporan_akurasi_tekanan->percobaan0_1_turun;
-$percobaan0_2_turun = $laporan_akurasi_tekanan->percobaan0_2_turun;
-$percobaan0_3_turun = $laporan_akurasi_tekanan->percobaan0_3_turun;
-$meanTurun0 = ($percobaan0_1_turun + $percobaan0_2_turun + $percobaan0_3_turun) / 3;
-
-$percobaan50_1_turun = $laporan_akurasi_tekanan->percobaan50_1_turun;
-$percobaan50_2_turun = $laporan_akurasi_tekanan->percobaan50_2_turun;
-$percobaan50_3_turun = $laporan_akurasi_tekanan->percobaan50_3_turun;
-$meanTurun50 = ($percobaan50_1_turun + $percobaan50_2_turun + $percobaan50_3_turun) / 3;
-
-$percobaan100_1_turun = $laporan_akurasi_tekanan->percobaan100_1_turun;
-$percobaan100_2_turun = $laporan_akurasi_tekanan->percobaan100_2_turun;
-$percobaan100_3_turun = $laporan_akurasi_tekanan->percobaan100_3_turun;
-$meanTurun100 = ($percobaan100_1_turun + $percobaan100_2_turun + $percobaan100_3_turun) / 3;
-
-$percobaan150_1_turun = $laporan_akurasi_tekanan->percobaan150_1_turun;
-$percobaan150_2_turun = $laporan_akurasi_tekanan->percobaan150_2_turun;
-$percobaan150_3_turun = $laporan_akurasi_tekanan->percobaan150_3_turun;
-$meanTurun150 = ($percobaan150_1_turun + $percobaan150_2_turun + $percobaan150_3_turun) / 3;
+$percobaan250_1_turun = $laporan_akurasi_tekanan->percobaan250_1_turun;
+$percobaan250_2_turun = $laporan_akurasi_tekanan->percobaan250_2_turun;
+$percobaan250_3_turun = $laporan_akurasi_tekanan->percobaan250_3_turun;
+$meanTurun250 = ($percobaan250_1_turun + $percobaan250_2_turun + $percobaan250_3_turun) / 3;
+$meanterkoreksiTurun250 = $data_sertifikat_akurasi_tekanan->intercept_turun + $data_sertifikat_akurasi_tekanan->x_variable_turun * $meanTurun250;
+// stdev
+$arr7= [];
+array_push($arr7, $percobaan250_1_turun, $percobaan250_2_turun, $percobaan250_3_turun);
+$stdev7 = standard_deviation($arr7);
+$koreksi7 = $meanTurun250 - 250;
 
 $percobaan200_1_turun = $laporan_akurasi_tekanan->percobaan200_1_turun;
 $percobaan200_2_turun = $laporan_akurasi_tekanan->percobaan200_2_turun;
 $percobaan200_3_turun = $laporan_akurasi_tekanan->percobaan200_3_turun;
 $meanTurun200 = ($percobaan200_1_turun + $percobaan200_2_turun + $percobaan200_3_turun) / 3;
+$meanterkoreksiTurun200 = $data_sertifikat_akurasi_tekanan->intercept_turun + $data_sertifikat_akurasi_tekanan->x_variable_turun * $meanTurun200;
+// stdev
+$arr8= [];
+array_push($arr8, $percobaan200_1_turun, $percobaan200_2_turun, $percobaan200_3_turun);
+$stdev8 = standard_deviation($arr8);
+$koreksi8 = $meanTurun200 - 200;
 
-$percobaan250_1_turun = $laporan_akurasi_tekanan->percobaan250_1_turun;
-$percobaan250_2_turun = $laporan_akurasi_tekanan->percobaan250_2_turun;
-$percobaan250_3_turun = $laporan_akurasi_tekanan->percobaan250_3_turun;
-$meanTurun250 = ($percobaan250_1_turun + $percobaan250_2_turun + $percobaan250_3_turun) / 3;
+$percobaan150_1_turun = $laporan_akurasi_tekanan->percobaan150_1_turun;
+$percobaan150_2_turun = $laporan_akurasi_tekanan->percobaan150_2_turun;
+$percobaan150_3_turun = $laporan_akurasi_tekanan->percobaan150_3_turun;
+$meanTurun150 = ($percobaan150_1_turun + $percobaan150_2_turun + $percobaan150_3_turun) / 3;
+$meanterkoreksiTurun150 = $data_sertifikat_akurasi_tekanan->intercept_turun + $data_sertifikat_akurasi_tekanan->x_variable_turun * $meanTurun150;
+// stdev
+$arr9= [];
+array_push($arr9, $percobaan150_1_turun, $percobaan150_2_turun, $percobaan150_3_turun);
+$stdev9 = standard_deviation($arr9);
+$koreksi9 = $meanTurun150 - 150;
+
+$percobaan100_1_turun = $laporan_akurasi_tekanan->percobaan100_1_turun;
+$percobaan100_2_turun = $laporan_akurasi_tekanan->percobaan100_2_turun;
+$percobaan100_3_turun = $laporan_akurasi_tekanan->percobaan100_3_turun;
+$meanTurun100 = ($percobaan100_1_turun + $percobaan100_2_turun + $percobaan100_3_turun) / 3;
+$meanterkoreksiTurun100 = $data_sertifikat_akurasi_tekanan->intercept_turun + $data_sertifikat_akurasi_tekanan->x_variable_turun * $meanTurun100;
+// stdev
+$arr10= [];
+array_push($arr10, $percobaan100_1_turun, $percobaan100_2_turun, $percobaan100_3_turun);
+$stdev10 = standard_deviation($arr10);
+$koreksi10 = $meanTurun100 - 100;
+
+$percobaan50_1_turun = $laporan_akurasi_tekanan->percobaan50_1_turun;
+$percobaan50_2_turun = $laporan_akurasi_tekanan->percobaan50_2_turun;
+$percobaan50_3_turun = $laporan_akurasi_tekanan->percobaan50_3_turun;
+$meanTurun50 = ($percobaan50_1_turun + $percobaan50_2_turun + $percobaan50_3_turun) / 3;
+$meanterkoreksiTurun50 = $data_sertifikat_akurasi_tekanan->intercept_turun + $data_sertifikat_akurasi_tekanan->x_variable_turun * $meanTurun50;
+// stdev
+$arr11= [];
+array_push($arr11, $percobaan50_1_turun, $percobaan50_2_turun, $percobaan50_3_turun);
+$stdev11 = standard_deviation($arr11);
+$koreksi11 = $meanTurun50 - 50;
+
+$percobaan0_1_turun = $laporan_akurasi_tekanan->percobaan0_1_turun;
+$percobaan0_2_turun = $laporan_akurasi_tekanan->percobaan0_2_turun;
+$percobaan0_3_turun = $laporan_akurasi_tekanan->percobaan0_3_turun;
+$meanTurun0 = ($percobaan0_1_turun + $percobaan0_2_turun + $percobaan0_3_turun) / 3;
+$meanterkoreksiTurun0 = $data_sertifikat_akurasi_tekanan->intercept_turun + $data_sertifikat_akurasi_tekanan->x_variable_turun * $meanTurun0;
+// stdev
+$arr12= [];
+array_push($arr12, $percobaan0_1_turun, $percobaan0_2_turun, $percobaan0_3_turun);
+$stdev12 = standard_deviation($arr12);
+$koreksi12 = $meanTurun0 - 00;
+
 
 ?>
 
@@ -345,7 +418,7 @@ $meanTurun250 = ($percobaan250_1_turun + $percobaan250_2_turun + $percobaan250_3
     <tbody>
         <tr>
             <td style="vertical-align: middle;text-align: center">260</td>
-            <td style="vertical-align: middle;text-align: center">{{ round($hitungLajuHasil,2)  }}</td>
+            <td style="vertical-align: middle;text-align: center">{{ round($hitungLajuHasil, 2) }}</td>
             <td style="vertical-align: middle;text-align: center"> <img src="../public/asset/kurang.png"
                     style="width: 6px; margin-top:3px"> 10 detik</td>
             <td style="vertical-align: middle;text-align: center">{{ $lajuHasil }}</td>
@@ -381,86 +454,148 @@ $meanTurun250 = ($percobaan250_1_turun + $percobaan250_2_turun + $percobaan250_3
             <td>{{ $percobaan0_1_naik }}</td>
             <td>{{ $percobaan0_2_naik }}</td>
             <td>{{ $percobaan0_3_naik }}</td>
-            <td>{{ round($meanNaik0,2) }}</td>
+            <td>{{ round($meanNaik0, 2) }}</td>
+            <td>{{ round($meanterkoreksi0, 2) }}</td>
+            <td>{{ round($stdev,2) }}</td>
+            <td>{{ round($koreksi,2) }}</td>
+            <td></td>
+            <td></td>
+            <td rowspan="12" style="text-align: center;vertical-align: middle;">4</td>
         </tr>
         <tr>
             <td>50</td>
             <td>{{ $percobaan50_1_naik }}</td>
             <td>{{ $percobaan50_2_naik }}</td>
             <td>{{ $percobaan50_3_naik }}</td>
-            <td>{{round($meanNaik50,2)}}</td>
+            <td>{{ round($meanNaik50, 2) }}</td>
+            <td>{{ round($meanterkoreksi50, 2) }}</td>
+            <td>{{ round($stdev2,2) }}</td>
+            <td>{{ round($koreksi2,2) }}</td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
             <td>100</td>
             <td>{{ $percobaan100_1_naik }}</td>
             <td>{{ $percobaan100_2_naik }}</td>
             <td>{{ $percobaan100_3_naik }}</td>
-            <td>{{round($meanNaik100,2)}}</td>
+            <td>{{ round($meanNaik100, 2) }}</td>
+            <td>{{ round($meanterkoreksi100, 2) }}</td>
+            <td>{{ round($stdev3,2) }}</td>
+            <td>{{ round($koreksi3,2) }}</td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
             <td>150</td>
             <td>{{ $percobaan150_1_naik }}</td>
             <td>{{ $percobaan150_2_naik }}</td>
             <td>{{ $percobaan150_3_naik }}</td>
-            <td>{{round($meanNaik150,2)}}</td>
+            <td>{{ round($meanNaik150, 2) }}</td>
+            <td>{{ round($meanterkoreksi150, 2) }}</td>
+            <td>{{ round($stdev4,2) }}</td>
+            <td>{{ round($koreksi4,2) }}</td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
             <td>200</td>
             <td>{{ $percobaan200_1_naik }}</td>
             <td>{{ $percobaan200_2_naik }}</td>
             <td>{{ $percobaan200_3_naik }}</td>
-            <td>{{round($meanNaik200,2)}}</td>
+            <td>{{ round($meanNaik200, 2) }}</td>
+            <td>{{ round($meanterkoreksi200, 2) }}</td>
+            <td>{{ round($stdev5,2) }}</td>
+            <td>{{ round($koreksi5,2) }}</td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
             <td>250</td>
             <td>{{ $percobaan250_1_naik }}</td>
             <td>{{ $percobaan250_2_naik }}</td>
             <td>{{ $percobaan250_3_naik }}</td>
-            <td>{{round($meanNaik250,2)}}</td>
+            <td>{{ round($meanNaik250, 2) }}</td>
+            <td>{{ round($meanterkoreksi250, 2) }}</td>
+            <td>{{ round($stdev6,2) }}</td>
+            <td>{{ round($koreksi6,2) }}</td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
             <td rowspan="6" style="background-color:grey;text-align: center;vertical-align: middle;"> <span
                     style="transform: rotate(-90deg);margin-top:60px; color:white"><b>Turun</b></span></td>
-            <td>0</td>
-            <td>{{ $percobaan0_1_turun }}</td>
-            <td>{{ $percobaan0_2_turun }}</td>
-            <td>{{ $percobaan0_3_turun }}</td>
-            <td>{{round($meanTurun0,2)}}</td>
-        </tr>
-        <tr>
-            <td>50</td>
-            <td>{{ $percobaan50_1_turun }}</td>
-            <td>{{ $percobaan50_2_turun }}</td>
-            <td>{{ $percobaan50_3_turun }}</td>
-            <td>{{round($meanTurun50,2)}}</td>
-        </tr>
-        <tr>
-            <td>100</td>
-            <td>{{ $percobaan100_1_turun }}</td>
-            <td>{{ $percobaan100_2_turun }}</td>
-            <td>{{ $percobaan100_3_turun }}</td>
-            <td>{{round($meanTurun100,2)}}</td>
-        </tr>
-        <tr>
-            <td>150</td>
-            <td>{{ $percobaan150_1_turun }}</td>
-            <td>{{ $percobaan150_2_turun }}</td>
-            <td>{{ $percobaan150_3_turun }}</td>
-            <td>{{round($meanTurun150,2)}}</td>
+            <td>250</td>
+            <td>{{ $percobaan250_1_turun }}</td>
+            <td>{{ $percobaan250_2_turun }}</td>
+            <td>{{ $percobaan250_3_turun }}</td>
+            <td>{{ round($meanTurun250, 2) }}</td>
+            <td>{{ round($meanterkoreksiTurun250, 2) }}</td>
+            <td>{{ round($stdev7,2) }}</td>
+            <td>{{ round($koreksi7,2) }}</td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
             <td>200</td>
             <td>{{ $percobaan200_1_turun }}</td>
             <td>{{ $percobaan200_2_turun }}</td>
             <td>{{ $percobaan200_3_turun }}</td>
-            <td>{{round($meanTurun200,2)}}</td>
+            <td>{{ round($meanTurun200, 2) }}</td>
+            <td>{{ round($meanterkoreksiTurun200, 2) }}</td>
+            <td>{{ round($stdev8,2) }}</td>
+            <td>{{ round($koreksi8,2) }}</td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
-            <td>250</td>
-            <td>{{ $percobaan250_1_turun }}</td>
-            <td>{{ $percobaan250_2_turun }}</td>
-            <td>{{ $percobaan250_3_turun }}</td>
-            <td>{{round($meanTurun250,2)}}</td>
+            <td>150</td>
+            <td>{{ $percobaan150_1_turun }}</td>
+            <td>{{ $percobaan150_2_turun }}</td>
+            <td>{{ $percobaan150_3_turun }}</td>
+            <td>{{ round($meanTurun150, 2) }}</td>
+            <td>{{ round($meanterkoreksiTurun150, 2) }}</td>
+            <td>{{ round($stdev9,2) }}</td>
+            <td>{{ round($koreksi9,2) }}</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>100</td>
+            <td>{{ $percobaan100_1_turun }}</td>
+            <td>{{ $percobaan100_2_turun }}</td>
+            <td>{{ $percobaan100_3_turun }}</td>
+            <td>{{ round($meanTurun100, 2) }}</td>
+            <td>{{ round($meanterkoreksiTurun100, 2) }}</td>
+            <td>{{ round($stdev10,2) }}</td>
+            <td>{{ round($koreksi10,2) }}</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>50</td>
+            <td>{{ $percobaan50_1_turun }}</td>
+            <td>{{ $percobaan50_2_turun }}</td>
+            <td>{{ $percobaan50_3_turun }}</td>
+            <td>{{ round($meanTurun50, 2) }}</td>
+            <td>{{ round($meanterkoreksiTurun50, 2) }}</td>
+            <td>{{ round($stdev11 ,2)}}</td>
+            <td>{{ round($koreksi11,2) }}</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+
+            <td>0</td>
+            <td>{{ $percobaan0_1_turun }}</td>
+            <td>{{ $percobaan0_2_turun }}</td>
+            <td>{{ $percobaan0_3_turun }}</td>
+            <td>{{ round($meanTurun0, 2) }}</td>
+            <td>{{ round($meanterkoreksiTurun0, 2) }}</td>
+            <td>{{ round($stdev12,2)  }}</td>
+            <td>{{ round($koreksi12,2) }}</td>
+            <td></td>
+            <td></td>
         </tr>
     </tbody>
 </table>
