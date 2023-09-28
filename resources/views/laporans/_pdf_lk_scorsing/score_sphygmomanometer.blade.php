c3b1bae0ccb52fef6b93b9cc5536b17a4e4f8b75
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-09-28 16:34:02 +0700

    update

--- resources/views/laporans/_pdf_lk_scorsing/score_sphygmomanometer.blade.php
+++ resources/views/laporans/_pdf_lk_scorsing/score_sphygmomanometer.blade.php
@@ -468,8 +468,7 @@ if ($score11 == 'Lulus') {
 if ($score12 == 'Lulus') {
     $initScore = $initScore + 1;
 }
-$initScore = ($initScore / $pembagi) * 100;
-$final = $initScore >= 70 ? 'Lulus' : 'Tidak';
+$initScore = (($initScore / $pembagi) * 100 / 2);
 ?>
 
 <p style="font-size: 11px;margin-left:18px"><b>1. CEK KEBOCORAN TEKANAN SETELAH 60 DETIK</b></p>
@@ -692,7 +691,6 @@ $final = $initScore >= 70 ? 'Lulus' : 'Tidak';
             <td>{{ $score11 }}</td>
         </tr>
         <tr>
-
             <td>0</td>
             <td>{{ $percobaan0_1_turun }}</td>
             <td>{{ $percobaan0_2_turun }}</td>
@@ -704,6 +702,135 @@ $final = $initScore >= 70 ? 'Lulus' : 'Tidak';
             <td>{{ round($u9512, 2) }}</td>
             <td>{{ round($absU9512, 2) }}</td>
             <td>{{ $score12 }}</td>
+        </tr>
+    </tbody>
+</table>
+
+
+{{-- telaah_teknis --}}
+<p style="font-size: 14px"><b>{{ $count_laporan_pengukuran_keselamatan_listrik > 0 ? 'G' : 'F' }}. TELAAH
+        TEKNIS</b></p>
+<table class="table table-bordered table-sm"
+    style="margin-left: 18px;font-size:11px;width:100%;margin-top:-10px; padding-right:18px">
+    <tbody>
+        @forelse ($laporan_telaah_teknis as $row)
+            <tr>
+                <td style="width: 4%;text-align: center;">{{ $loop->iteration }}</td>
+                <td style="text-align: justify;vertical-align: middle;">{{ $row->field_telaah_teknis }}</td>
+                <td>
+                    <div class="form-group" style="margin: 0px">
+                        <input type="checkbox" {{ $row->value == 'baik' ? 'checked' : '' }}>
+                        <label>Baik</label>
+                    </div>
+                    <div class="form-group" style="margin: 0px">
+                        <input type="checkbox" {{ $row->value == 'tidak-baik' ? 'checked' : '' }}>
+                        <label>Tidak Baik</label>
+                    </div>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td style="text-align: center;">-</td>
+                <td style="text-align: center;">-</td>
+                <td style="text-align: center;">-</td>
+            </tr>
+        @endforelse
+    </tbody>
+</table>
+
+<table class="table table-bordered table-sm"
+    style="margin-left: 18px;font-size:11px;width:100%;margin-top:-10px; padding-right:18px">
+    <thead>
+        <tr>
+            <th style="width: 4%;text-align: center;">No</th>
+            <th style="width: 24%;text-align: center;">Parameter</th>
+            <th style="width: 24%;text-align: center;">Skor</th>
+            <th style="width: 24%;text-align: center;">Total</th>
+            @php
+                $totalAll = $score_fisik + $scoreTekananHasil + $scoreLajuHasil + $initScore;
+            @endphp
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td style="text-align: center;">1</td>
+            <td style="text-align: center;">PEMERIKSAAAN KONDISI FISIK DAN FUNGSI</td>
+            <td style="text-align: center;">{{ $score_fisik }}</td>
+            <td style="text-align: center;vertical-align: middle;" rowspan="3">
+                {{ $totalAll }}
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">2</td>
+            <td style="text-align: center;">CEK KEBOCORAN</td>
+            <td style="text-align: center;">
+                {{ $scoreTekananHasil }}
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">3</td>
+            <td style="text-align: center;">LAJU BUANG CEPAT</td>
+            <td style="text-align: center;">
+                {{ $scoreLajuHasil }}
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;">4</td>
+            <td style="text-align: center;">KALIBRASI AKURASI TEKANAN</td>
+            <td style="text-align: center;">
+                {{ $initScore }}
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+<table class="table table-bordered table-sm"
+    style="margin-left: 18px;font-size:11px;width:100%;margin-top:-10px; padding-right:18px">
+    <tbody>
+        <tr>
+            <td style="height:60px"><b>Catatan :</b> {{ $laporan_kesimpulan_telaah_teknis->catatan }} </td>
+        </tr>
+    </tbody>
+</table>
+
+
+<div class="page_break"></div>
+<table class="table table-bordered table-sm"
+    style="margin-left: 18px;font-size:11px;width:100%;margin-top:-10px; padding-right:18px">
+    <tbody>
+        <tr>
+            <td style="width: 40%;text-align: center;vertical-align: middle;">Berdasarkan hasil pengujian dan/ atau
+                hasil kalibrasi, alat ini dinyatakan </td>
+            <td style="width: 20%;text-align: center;vertical-align: middle;">
+                <div class="form-group" style="margin: 0px">
+                    <input type="checkbox" {{ $totalAll >= 70 ? 'checked' : '' }}>
+                    <label><b style="font-size: 12px">LAIK PAKAI</b></label>
+                </div>
+            </td>
+            <td style="width: 20%;text-align: center;vertical-align: middle;">
+                <div class="form-group" style="margin: 0px">
+                    <input type="checkbox" {{ $totalAll < 70 ? 'checked' : '' }}>
+                    <label><b style="font-size: 12px">TIDAK LAIK PAKAI</b></label>
+                </div>
+            </td>
+            <td style="width: 20%;text-align: center;vertical-align: middle;"><b style="font-size: 12px">PENYELIA</b>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;height:75px;vertical-align: middle;">Pelaksana Pengujian dan Kalibrasi
+            </td>
+            <td colspan="2" style="text-align: center">
+                <img style="width: 80px;margin-top:5px;margin-bottom:3px"
+                    src="data:image/png;base64, {!! base64_encode(QrCode::generate($laporan->nama_teknisi)) !!} "> <br>
+                <span>{{ $laporan->nama_teknisi }}</span>
+            </td>
+            <td style="text-align: center">
+                @if (isset($laporan->name_user))
+                    <img style="width: 80px;margin-top:5px;margin-bottom:3px"
+                        src="data:image/png;base64, {!! base64_encode(QrCode::generate($laporan->name_user)) !!} "> <br>
+                    <span>{{ $laporan->name_user }}</span>
+                @endif
+            </td>
         </tr>
     </tbody>
 </table>
